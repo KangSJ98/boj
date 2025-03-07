684b4a85c594c8ad8b012ee344066cf65bdadc99
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2025-03-08 02:17:58 +0900

    [Silver IV] Title: 베스킨라빈스 31, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/20004. 베스킨라빈스 31/베스킨라빈스 31.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    for(int i=1;i<=a;i++)
+    {
+        if (30 % (i+1) == 0)
+            cout<<i<<"\n";
+    }
+}
