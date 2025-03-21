79348676f1af1780c8dfd22abf1d2955a11558d7
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2025-03-21 10:18:12 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -1,37 +1,30 @@
 #include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0);cin.tie(0)
-#define rep(i,a,b) for(int i=(a);i<(b);i++)
-
 using namespace std;
 
-int t;
-int hochul;
-bool isPal;
+int cnt;
+
+int recursion(const string &s, int l, int r) {
+    cnt++;
+    if (l >= r) return 1;
+    if (s[l] != s[r]) return 0;
+    return recursion(s, l + 1, r - 1);
+}
+
+int isPalindrome(const string &s) {
+    return recursion(s, 0, s.size() - 1);
+}
 
 int main(){
-    cin>>t;
-    while(t--){
-        string tmp;
-        cin>>tmp;
-        hochul = 1;
-        isPal = true;
-
-        rep(i, 0, size(tmp) / 2){
-            if (tmp[i] == tmp[size(tmp) - 1 - i]){
-                hochul++;
-            }
-            else{
-                isPal = false;
-                break;
-            }
-        }
-
-        if(isPal){
-            cout<<1<<" "<<hochul<<endl;
-        }
-        else{
-            cout<<0<<" "<<hochul<<endl;
-        }
-
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int T;
+    cin >> T;
+    while(T--){
+        string S;
+        cin >> S;
+        cnt = 0;
+        int result = isPalindrome(S);
+        cout << result << " " << cnt << "\n";
     }
 }
