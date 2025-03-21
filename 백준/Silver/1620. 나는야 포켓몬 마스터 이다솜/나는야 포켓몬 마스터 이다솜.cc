1d4bf0db8b1839e580885fd47416399dbcd4caf4
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2025-03-21 17:03:09 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 156 ms, Memory: 23424 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+
+using namespace std;
+
+int n, m;
+
+vector<string> num2pocket;
+map<string, string> pokcet2num;
+
+bool isNumber(string str) {
+    if(str[0]>='A' && str[0] <= 'Z'){
+        return false;
+    }
+    return !str.empty();
+}
+
+int main(void){
+    fastio;
+    cin>>n>>m;
+    rep(i, 1, n+1){
+        string tmp;
+        cin>>tmp;
+        num2pocket.push_back(tmp);
+        pokcet2num[tmp] = to_string(i);
+    }
+    while(m--){
+        string tmp;
+        cin>>tmp;
+        if (isNumber(tmp)){         
+            cout<<num2pocket[stoi(tmp) - 1]<<"\n";
+        }
+        else{
+            cout<<pokcet2num[tmp]<<"\n";
+        }
+    }
+}
