c0f088743d7c5f39cec147829c2fa93e6854df6d
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2025-04-02 14:55:19 +0900

    [Gold V] Title: RUN Number, Time: 124 ms, Memory: 110832 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31813. RUN Number/RUN Number.py
+++ 백준/Gold/31813. RUN Number/RUN Number.py
@@ -1,31 +1,35 @@
 t = int(input())
 
+def find_max_run(n):
+    max_run = 0
+    for i in range(1, 10):
+        tmp = i
+        j = 1
+        while True:
+            if (tmp  + i * pow(10, j) < n):
+                tmp += i * pow(10, j)
+                j += 1
+            else:
+                break
+        
+        max_run = max(max_run, tmp)
+    
+    return max_run
+
+
 while t:
     t -= 1
 
     n, k = map(int, input().split())
     run_number = []
 
-    while k != 0:
-        lenk = len(str(k))
-        if (len(str(k)) == 1) or (int(str(k)[0]) <= int(str(k)[1])):
-            tmp = int(str(k)[0])
-        else:
-            tmp = int(str(k)[0]) - 1
-        
-        if tmp == 0:
-            tmp = 9
+    while k >= 10:
+        tmp_run = find_max_run(k)
+        run_number.append(tmp_run)
+        k -= tmp_run
+    
+    run_number.append(k)
 
-        run_tmp = 0
-        for i in range(lenk):
-            run_tmp += tmp * (pow(10, i))
-            if run_tmp > k:
-                run_tmp -= tmp * (pow(10, i))
-                break
-        
-        run_number.append(run_tmp)
-
-        k -= run_tmp
 
     print(len(run_number))
     for data in run_number:
