5e670989915a2030761e837071444835f685ccaf
SungJun Kang <130119769+KangSJ98@users.noreply.github.com>
2025-06-04 16:40:11 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 128 ms, Memory: 112612 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(5 * n):
+        print("@",end="")
+    print()
+
+for i in range(n):
+    for j in range(n):
+        print("@",end="")
+    print()
+
+for i in range(n):
+    for j in range(5 * n):
+        print("@",end="")
+    print()
+    
+for i in range(n):
+    for j in range(n):
+        print("@",end="")
+    print()
+    
+for i in range(n):
+    for j in range(5 * n):
+        print("@",end="")
+    print()
+    
